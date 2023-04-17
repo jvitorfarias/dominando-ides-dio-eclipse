6211a94d2b033f668b68272e84f0284a2a7316bc
jvitorfarias <joaovitorfarias4@gmail.com>
2023-04-17 20:28:59 -0300

    creating calculator

---
+++ src/primeiro/programa/calculadora/Calculadora.java
@@ -0,0 +1,46 @@
+package primeiro.programa.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		double a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(multiplicacao);
+		System.out.println(divisao);
+	
+		scan.close();
+	}
+
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+	
+	public static double subtracao(double a, double b) {
+		return a - b;
+	}
+	
+	public static double divisao(double a, double b) {
+		return a / b;
+	}
+	
+	public static double multiplicacao(double a, double b) {
+		return a * b;
+	}
+	
+}
